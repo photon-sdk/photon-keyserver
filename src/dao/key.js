c05eb6df5194eb96d7b5229a02821ad68e34e833
Tankred Hase <mail@tankredhase.de>
2020-03-24 08:43:30 +0800

    Refactor key dao

--- src/dao/key.js
+++ src/dao/key.js
@@ -11,14 +11,12 @@ const { v4: uuid } = require('uuid');
  * Database documents have the format:
  * {
  *   id: '550e8400-e29b-11d4-a716-446655440000', // a randomly generated UUID
- *   encryptionKey: '4d3958fd5f662c37b8e4105cc9dedc77',
+ *   encryptionKey: 'e9e0e7159c57ad8298c9faf3df73aa04d792a3769a2e294ef91fa6cb8858338d',
  * }
  */
-const TableName = process.env.DYNAMODB_TABLE_KEY;
-
+const TABLE = process.env.DYNAMODB_TABLE_KEY;
 
 class Key {
-
   constructor(dynamo) {
     this._dynamo = dynamo;
   }
@@ -26,26 +24,15 @@ class Key {
   async create() {
     const id = uuid();
     const encryptionKey = await this._generateKey();
-    await this._dynamo.create(
-      {
-        TableName,
-        Item: {
-          id,
-          encryptionKey,
-        }
-      }
-    );
+    await this._dynamo.put(TABLE, {
+      id,
+      encryptionKey,
+    });
     return id;
   }
 
   async get({ id }) {
-    const doc = await this._dynamo.get({
-      TableName,
-      Key: {
-        id
-      },
-    });
-    return doc.Item;
+    return this._dynamo.get(TABLE, { id });
   }
 
   async _generateKey() {
@@ -59,7 +46,6 @@ class Key {
       });
     });
   }
-
 }
 
 module.exports = Key;
