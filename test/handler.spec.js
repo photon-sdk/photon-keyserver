06840852b8c2743a77de5b80d0b32363e50a8414
Tankred Hase <mail@tankredhase.de>
2020-03-29 17:57:56 +0800

    Refactor handler integration test to use unexpected

--- test/handler.spec.js
+++ test/handler.spec.js
@@ -1,14 +1,13 @@
 /* eslint-env mocha */
-/* eslint no-unused-expressions: "off" */
 
 'use strict'
 
+const expect = require('unexpected')
 const mochaPlugin = require('serverless-mocha-plugin')
-const dynamo = require('../src/service/dynamodb')
-const expect = mochaPlugin.chai.expect
 const createKey = mochaPlugin.getWrapper('createKey', '/handler.js', 'createKey')
 const verifyKey = mochaPlugin.getWrapper('verifyKey', '/handler.js', 'verifyKey')
 const getKey = mochaPlugin.getWrapper('getKey', '/handler.js', 'getKey')
+const dynamo = require('../src/service/dynamodb')
 
 describe('handlers', () => {
   const TABLE_USER = process.env.DYNAMODB_TABLE_USER
@@ -22,85 +21,85 @@ describe('handlers', () => {
   })
 
   describe('createKey', () => {
-    it('should handle empty body', async () => {
+    it('handle empty body', async () => {
       const response = await createKey.run({})
-      expect(response.statusCode).to.equal(400)
+      expect(response.statusCode, 'to be', 400)
     })
 
-    it('should create key document', async () => {
+    it('create key document', async () => {
       const response = await createKey.run({
         body: JSON.stringify({ phone })
       })
       keyId = JSON.parse(response.body).id
-      expect(keyId).to.not.be.empty
-      expect(response.statusCode).to.equal(201)
+      expect(keyId, 'to be ok')
+      expect(response.statusCode, 'to be', 201)
     })
   })
 
   describe('getKey', () => {
-    it('should handle empty query params', async () => {
+    it('handle empty query params', async () => {
       const response = await getKey.run({
         pathParameters: null,
         queryStringParameters: null
       })
-      expect(response.statusCode).to.equal(400)
+      expect(response.statusCode, 'to be', 400)
     })
 
-    it('should not find unverified number', async () => {
+    it('not find unverified number', async () => {
       const response = await getKey.run({
         pathParameters: { keyId },
         queryStringParameters: { phone: encodeURIComponent(phone) }
       })
-      expect(response.statusCode).to.equal(404)
+      expect(response.statusCode, 'to be', 404)
     })
   })
 
   describe('verifyKey', () => {
     before(async () => {
       code1 = (await dynamo.get(TABLE_USER, { id: phone })).code
-      expect(code1).to.not.be.empty
+      expect(code1, 'to be ok')
     })
 
-    it('should handle empty body', async () => {
+    it('handle empty body', async () => {
       const response = await verifyKey.run({})
-      expect(response.statusCode).to.equal(400)
+      expect(response.statusCode, 'to be', 400)
     })
 
-    it('should set user ID as verified', async () => {
+    it('set user ID as verified', async () => {
       const response = await verifyKey.run({
         pathParameters: { keyId },
         body: JSON.stringify({ phone, code: code1 })
       })
       const key = JSON.parse(response.body)
-      expect(key.id).to.equal(keyId)
-      expect(Buffer.from(key.encryptionKey, 'hex').length).to.equal(32)
-      expect(response.statusCode).to.equal(200)
+      expect(key.id, 'to be', keyId)
+      expect(Buffer.from(key.encryptionKey, 'hex').length, 'to be', 32)
+      expect(response.statusCode, 'to be', 200)
     })
   })
 
   describe('getKey', () => {
-    it('should read key document', async () => {
+    it('read key document', async () => {
       const response = await getKey.run({
         pathParameters: { keyId },
         queryStringParameters: { phone: encodeURIComponent(phone) }
       })
-      expect(response.statusCode).to.equal(200)
+      expect(response.statusCode, 'to be', 200)
     })
   })
 
   describe('verifyKey', () => {
     before(async () => {
       code2 = (await dynamo.get(TABLE_USER, { id: phone })).code
-      expect(code2).to.not.be.empty
+      expect(code2, 'to be ok')
     })
 
-    it('should verify a different code', async () => {
-      expect(code1).to.not.equal(code2)
+    it('verify a different code', async () => {
+      expect(code1, 'not to be', code2)
       const response = await verifyKey.run({
         pathParameters: { keyId },
         body: JSON.stringify({ phone, code: code2 })
       })
-      expect(response.statusCode).to.equal(200)
+      expect(response.statusCode, 'to be', 200)
     })
   })
 })
