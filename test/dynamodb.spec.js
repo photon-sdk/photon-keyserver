b2fcef6f387450c26e2d994b9a48b179edbadb92
Tankred Hase <mail@tankredhase.de>
2020-03-30 16:44:10 +0800

    Implement dynamodb integration tests

---
+++ test/dynamodb.spec.js
@@ -0,0 +1,52 @@
+/* eslint-env mocha */
+
+'use strict'
+
+const expect = require('unexpected')
+const dynamo = require('../src/service/dynamodb')
+
+describe('DynamoDB Service integration test', () => {
+  const TABLE = process.env.DYNAMODB_TABLE_KEY
+
+  before(async () => {
+    dynamo.init()
+  })
+
+  describe('put', () => {
+    it('store a new item', async () => {
+      const response = await dynamo.put(TABLE, { id: 'foo' })
+      expect(response, 'to be ok')
+    })
+
+    it('overwrite an existing item', async () => {
+      const response = await dynamo.put(TABLE, { id: 'foo', bar: 'baz' })
+      expect(response, 'to be ok')
+    })
+
+    it('fail on invalid args', async () => {
+      await expect(dynamo.put(), 'to be rejected with', /Missing/)
+    })
+  })
+
+  describe('get', () => {
+    it('read item by id', async () => {
+      const item = await dynamo.get(TABLE, { id: 'foo' })
+      expect(item.bar, 'to equal', 'baz')
+    })
+
+    it('fail on invalid args', async () => {
+      await expect(dynamo.get(), 'to be rejected with', /Missing/)
+    })
+  })
+
+  describe('remove', () => {
+    it('read item by id', async () => {
+      const response = await dynamo.remove(TABLE, { id: 'foo' })
+      expect(response, 'to be ok')
+    })
+
+    it('fail on invalid args', async () => {
+      await expect(dynamo.remove(), 'to be rejected with', /Missing/)
+    })
+  })
+})
