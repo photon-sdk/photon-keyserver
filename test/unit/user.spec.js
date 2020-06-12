985417e051840e8d033682c75f385b71270af063
Tankred Hase <mail@tankredhase.de>
2020-06-12 10:08:07 +0800

    Add unit tests for pin support in user dao

--- test/unit/user.spec.js
+++ test/unit/user.spec.js
@@ -15,6 +15,9 @@ describe('User DAO unit test', () => {
   const op = 'read'
   const code1 = '123456'
   const code2 = '654321'
+  const pin = '1234'
+  const salt = 'Brtp4M8nqbezABqTvbgM/wLmiq+/wqYXakEcx1VrqpY='
+  const reBase64 = /^(?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{2}==|[A-Za-z0-9+/]{3}=)?$/
 
   beforeEach(() => {
     sandbox = sinon.createSandbox()
@@ -30,21 +33,63 @@ describe('User DAO unit test', () => {
       await expect(userDao.create({}), 'to be rejected with', /Invalid/)
     })
 
+    it('fail on invalid pin', async () => {
+      await expect(userDao.create({ phone, keyId, pin: '123' }), 'to be rejected with', /Invalid/)
+    })
+
     it('fail on dynamo error', async () => {
       dynamo.put.rejects(new Error('boom'))
       await expect(userDao.create({ phone, keyId }), 'to be rejected with', /boom/)
     })
 
-    it('store a new user', async () => {
+    it('store a new user (no pin)', async () => {
       const code = await userDao.create({ phone, keyId })
       expect(code, 'to match', /^\d{6}$/)
       expect(dynamo.put.callCount, 'to equal', 1)
+    })
+
+    it('store a new user (with pin)', async () => {
+      const code = await userDao.create({ phone, keyId, pin })
+      expect(code, 'to match', /^\d{6}$/)
+      expect(dynamo.put.callCount, 'to equal', 1)
+      sinon.assert.calledWith(dynamo.put, sinon.match.any, {
+        id: sinon.match(reBase64),
+        type: 'phone',
+        keyId,
+        op: 'verify',
+        code: sinon.match(/^\d{6}$/),
+        pin: sinon.match(reBase64),
+        salt: sinon.match(reBase64),
+        verified: false,
+        firstInvalid: null,
+        invalidCount: 0
+      })
     })
   })
 
   describe('verify', () => {
     it('fail on invalid args', async () => {
       await expect(userDao.verify({}), 'to be rejected with', /Invalid/)
+    })
+
+    it('fail on invalid pin', async () => {
+      await expect(userDao.verify({
+        phone,
+        keyId,
+        op,
+        code: code1,
+        pin: '123'
+      }), 'to be rejected with', /Invalid/)
+    })
+
+    it('fail on invalid newPin', async () => {
+      await expect(userDao.verify({
+        phone,
+        keyId,
+        op,
+        code: code1,
+        newPin: '123'
+      }), 'to be rejected with', /Invalid/)
     })
 
     it('return null if no user is found', async () => {
@@ -67,10 +112,10 @@ describe('User DAO unit test', () => {
       expect(user, 'to be', null)
       expect(delay, 'to be', null)
       expect(dynamo.put.callCount, 'to equal', 1)
-      expect(dynamo.put.calledWithMatch(sinon.match.any, {
+      sinon.assert.calledWithMatch(dynamo.put, sinon.match.any, {
         invalidCount: 10,
         firstInvalid: sinon.match.string
-      }), 'to be ok')
+      })
     })
 
     it('rate limit brute forcing of incorrect code', async () => {
@@ -85,10 +130,10 @@ describe('User DAO unit test', () => {
       expect(user, 'to be', null)
       expect(verify.isDateISOString(delay), 'to be', true)
       expect(dynamo.put.callCount, 'to equal', 1)
-      expect(dynamo.put.calledWithMatch(sinon.match.any, {
+      sinon.assert.calledWithMatch(dynamo.put, sinon.match.any, {
         invalidCount: 11,
         firstInvalid: sinon.match.string
-      }), 'to be ok')
+      })
     })
 
     it('rate limit brute forcing of correct code', async () => {
@@ -103,10 +148,10 @@ describe('User DAO unit test', () => {
       expect(user, 'to be', null)
       expect(verify.isDateISOString(delay), 'to be', true)
       expect(dynamo.put.callCount, 'to equal', 1)
-      expect(dynamo.put.calledWithMatch(sinon.match.any, {
+      sinon.assert.calledWithMatch(dynamo.put, sinon.match.any, {
         invalidCount: 11,
         firstInvalid: sinon.match.string
-      }), 'to be ok')
+      })
     })
 
     it('reset rate limit after time delay is over', async () => {
@@ -122,10 +167,10 @@ describe('User DAO unit test', () => {
       expect(user, 'to be', null)
       expect(delay, 'to be', null)
       expect(dynamo.put.callCount, 'to equal', 1)
-      expect(dynamo.put.calledWithMatch(sinon.match.any, {
+      sinon.assert.calledWithMatch(dynamo.put, sinon.match.any, {
         invalidCount: 0,
         firstInvalid: null
-      }), 'to be ok')
+      })
     })
 
     it('not verify a user with incorrect keyId', async () => {
@@ -170,11 +215,13 @@ describe('User DAO unit test', () => {
       await expect(userDao.verify({ phone, keyId, op, code: code1 }), 'to be rejected with', /boom/)
     })
 
-    it('verify a user with correct code', async () => {
+    it('verify a user with correct code (no pin)', async () => {
       dynamo.get.resolves({
         keyId,
         op,
         code: code1,
+        pin: null,
+        salt,
         verified: false,
         invalidCount: 1
       })
@@ -183,10 +230,63 @@ describe('User DAO unit test', () => {
       expect(user.code, 'not to be', code1)
       expect(delay, 'to be', undefined)
       expect(dynamo.put.callCount, 'to equal', 2)
-      expect(dynamo.put.calledWithMatch(sinon.match.any, {
+      sinon.assert.calledWithMatch(dynamo.put, sinon.match.any, {
         invalidCount: 0,
         firstInvalid: null
-      }), 'to be ok')
+      })
+    })
+
+    it('verify a user with correct code (with pin)', async () => {
+      dynamo.get.resolves({
+        keyId,
+        op,
+        code: code1,
+        pin: '5nTbjDe/u9TjtpPZ0O7rgMQ2BRhmhzqJqoUqX/d0pyk=',
+        salt,
+        verified: false,
+        invalidCount: 1
+      })
+      const { user, delay } = await userDao.verify({ phone, keyId, op, code: code1, pin })
+      expect(user.verified, 'to be', true)
+      expect(user.code, 'not to be', code1)
+      expect(delay, 'to be', undefined)
+      expect(dynamo.put.callCount, 'to equal', 2)
+      sinon.assert.calledWithMatch(dynamo.put, sinon.match.any, {
+        op: null,
+        verified: true,
+        invalidCount: 0,
+        firstInvalid: null
+      })
+    })
+
+    it('verify a user with correct code (with newPin)', async () => {
+      dynamo.get.resolves({
+        keyId,
+        op,
+        code: code1,
+        pin: null,
+        salt,
+        verified: false,
+        invalidCount: 1
+      })
+      const { user, delay } = await userDao.verify({
+        phone,
+        keyId,
+        op,
+        code: code1,
+        newPin: pin
+      })
+      expect(user.verified, 'to be', true)
+      expect(user.code, 'not to be', code1)
+      expect(delay, 'to be', undefined)
+      expect(dynamo.put.callCount, 'to equal', 2)
+      sinon.assert.calledWithMatch(dynamo.put, sinon.match.any, {
+        op: null,
+        pin: '5nTbjDe/u9TjtpPZ0O7rgMQ2BRhmhzqJqoUqX/d0pyk=',
+        verified: true,
+        invalidCount: 0,
+        firstInvalid: null
+      })
     })
   })
 
@@ -290,14 +390,14 @@ describe('User DAO unit test', () => {
       const code = await userDao.setNewCode({ phone, keyId, op })
       expect(code, 'to match', /^\d{6}$/)
       expect(dynamo.put.callCount, 'to equal', 1)
-      expect(dynamo.put.calledWithMatch(sinon.match.any, {
+      sinon.assert.calledWithMatch(dynamo.put, sinon.match.any, {
         keyId,
         op,
         code,
         verified: true,
         firstInvalid: '2020-06-01T03:33:47.980Z',
         invalidCount: 6
-      }), 'to be ok')
+      })
     })
   })
 
