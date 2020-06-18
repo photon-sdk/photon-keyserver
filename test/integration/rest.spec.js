ff8f8c1bf4d66fe3d8f0d1544a7db82ddb5fcbe9
Tankred Hase <mail@tankredhase.de>
2020-06-18 15:40:10 +0800

    Write rest integration tests for new api

--- test/integration/rest.spec.js
+++ test/integration/rest.spec.js
@@ -4,6 +4,7 @@
 
 const Frisbee = require('frisbee')
 const expect = require('unexpected')
+const keyDao = require('../../src/dao/key')
 const userDao = require('../../src/dao/user')
 const dynamo = require('../../src/service/dynamodb')
 
@@ -16,12 +17,11 @@ describe('REST api integration test', () => {
   let code1
   let code2
   let code3
-  let code4
 
   before(async () => {
     dynamo.init()
     client = new Frisbee({
-      baseURI: 'http://localhost:3000',
+      baseURI: 'http://localhost:3000/dev',
       headers: {
         Accept: 'application/json',
         'Content-Type': 'application/json'
@@ -29,18 +29,24 @@ describe('REST api integration test', () => {
     })
   })
 
-  describe('POST: upload new key', () => {
-    it('handle empty body', async () => {
-      const response = await client.post('/dev/v1/key', {
-        body: {}
+  after(async () => {
+    const { success } = await keyDao.remove({ id: keyId, pin: pin2 })
+    expect(success, 'to be', true)
+  })
+
+  describe('POST: create new key', () => {
+    it('handle invalid pin', async () => {
+      const response = await client.post('/v1/key', {
+        body: {
+          pin: '123'
+        }
       })
       expect(response.status, 'to be', 400)
     })
 
     it('create key document', async () => {
-      const response = await client.post('/dev/v1/key', {
+      const response = await client.post('/v1/key', {
         body: {
-          phone,
           pin: pin1
         }
       })
@@ -50,20 +56,99 @@ describe('REST api integration test', () => {
     })
   })
 
-  describe('GET: request unverified key', () => {
-    it('handle empty query params', async () => {
-      const response = await client.get(`/dev/v1/key/${keyId}`)
+  describe('GET: read key', () => {
+    it('handle empty auth headers', async () => {
+      client.auth()
+      const response = await client.get(`/v1/key/${keyId}`)
       expect(response.status, 'to be', 400)
     })
 
-    it('not find unverified number', async () => {
-      const response = await client.get(`/dev/v1/key/${keyId}`, {
-        params: {
+    it('read encryption key', async () => {
+      client.auth('', pin1)
+      const response = await client.get(`/v1/key/${keyId}`)
+      expect(response.status, 'to be', 200)
+      const { id, encryptionKey } = response.body
+      expect(id, 'to be', keyId)
+      expect(Buffer.from(encryptionKey, 'base64').length, 'to be', 32)
+    })
+  })
+
+  describe('PUT: change pin', () => {
+    it('handle invalid new pin', async () => {
+      const response = await client.put(`/v1/key/${keyId}`, {
+        body: {
+          pin: pin1,
+          newPin: '567'
+        }
+      })
+      expect(response.status, 'to be', 400)
+    })
+
+    it('should not find with wrong pin', async () => {
+      const response = await client.put(`/v1/key/${keyId}`, {
+        body: {
+          pin: pin2,
+          newPin: pin2
+        }
+      })
+      expect(response.status, 'to be', 404)
+    })
+
+    it('change to another pin', async () => {
+      const response = await client.put(`/v1/key/${keyId}`, {
+        body: {
+          pin: pin1,
+          newPin: pin2
+        }
+      })
+      expect(response.status, 'to be', 200)
+    })
+  })
+
+  describe('GET: read key with new pin', () => {
+    it('old pin should not work anymore', async () => {
+      client.auth('', pin1)
+      const response = await client.get(`/v1/key/${keyId}`)
+      expect(response.status, 'to be', 404)
+    })
+
+    it('read key with new pin', async () => {
+      client.auth('', pin2)
+      const response = await client.get(`/v1/key/${keyId}`)
+      expect(response.status, 'to be', 200)
+      expect(response.body.encryptionKey, 'to be ok')
+    })
+  })
+
+  describe('POST: create new user', () => {
+    it('return 400 for invalid phone number', async () => {
+      const response = await client.post(`/v1/key/${keyId}/user`, {
+        body: {
+          phone: 'invalid',
+          pin: pin2
+        }
+      })
+      expect(response.status, 'to be', 400)
+    })
+
+    it('old pin should not work anymore', async () => {
+      const response = await client.post(`/v1/key/${keyId}/user`, {
+        body: {
           phone,
           pin: pin1
         }
       })
       expect(response.status, 'to be', 404)
+    })
+
+    it('should create new user', async () => {
+      const response = await client.post(`/v1/key/${keyId}/user`, {
+        body: {
+          phone,
+          pin: pin2
+        }
+      })
+      expect(response.status, 'to be', 201)
     })
 
     after(async () => {
@@ -72,151 +157,92 @@ describe('REST api integration test', () => {
     })
   })
 
-  describe('PUT: verify upload and download key', () => {
-    it('handle empty body', async () => {
-      const response = await client.put(`/dev/v1/key/${keyId}`, {
-        body: {}
+  describe('PUT: verify new user', () => {
+    it('return 400 for invalid op', async () => {
+      const response = await client.put(`/v1/key/${keyId}/user/${phone}`, {
+        body: {
+          code: code1,
+          op: 'invalid-op'
+        }
       })
       expect(response.status, 'to be', 400)
     })
 
-    it('set user ID as verified', async () => {
-      const response = await client.put(`/dev/v1/key/${keyId}`, {
+    it('verify user with correct op', async () => {
+      const response = await client.put(`/v1/key/${keyId}/user/${phone}`, {
         body: {
-          phone,
           code: code1,
-          op: 'verify',
-          pin: pin1
+          op: 'verify'
         }
       })
       expect(response.status, 'to be', 200)
-      const key = response.body
-      expect(key.id, 'to be', keyId)
-      expect(Buffer.from(key.encryptionKey, 'base64').length, 'to be', 32)
     })
   })
 
-  describe('GET: request verified key', () => {
-    it('read key document', async () => {
-      const response = await client.get(`/dev/v1/key/${keyId}`, {
-        params: { phone }
+  describe('POST: create user again', () => {
+    it('should return 409 if user id already exists', async () => {
+      const response = await client.post(`/v1/key/${keyId}/user`, {
+        body: {
+          phone,
+          pin: pin2
+        }
       })
+      expect(response.status, 'to be', 409)
+    })
+  })
+
+  describe('GET: reset pin using verified user', () => {
+    before(() => {
+      client.auth()
+    })
+
+    it('return 400 for invalid key id', async () => {
+      const response = await client.get(`/v1/key/invalid-key-id/user/${phone}/reset`)
+      expect(response.status, 'to be', 400)
+    })
+
+    it('return 404 for wrong user id', async () => {
+      const response = await client.get(`/v1/key/${keyId}/user/+4917512345679/reset`)
+      expect(response.status, 'to be', 404)
+    })
+
+    it('should request reset pin', async () => {
+      const response = await client.get(`/v1/key/${keyId}/user/${phone}/reset`)
       expect(response.status, 'to be', 200)
-      expect(response.body.message, 'to be', 'Success')
     })
 
     after(async () => {
       code2 = (await userDao.get({ phone })).code
       expect(code2, 'to be ok')
-      expect(code1, 'not to be', code2)
+      expect(code2, 'not to be', code1)
     })
   })
 
-  describe('PUT: verify read request and download key', () => {
-    it('verify a different code', async () => {
-      const response = await client.put(`/dev/v1/key/${keyId}`, {
+  describe('PUT: verify pin reset', () => {
+    it('set time lock on key for 30 days', async () => {
+      const response = await client.put(`/v1/key/${keyId}/user/${phone}`, {
         body: {
-          phone,
           code: code2,
-          op: 'read',
-          pin: pin1
+          op: 'reset-pin'
         }
       })
-      expect(response.status, 'to be', 200)
-      expect(response.body.encryptionKey, 'to be ok')
+      expect(response.status, 'to be', 423)
+      expect(response.body.message, 'to match', /locked until/)
+      expect(response.body.delay, 'to be ok')
     })
   })
 
-  describe('GET: request key to change pin', () => {
-    it('read key document', async () => {
-      const response = await client.get(`/dev/v1/key/${keyId}`, {
-        params: { phone }
-      })
-      expect(response.status, 'to be', 200)
-      expect(response.body.message, 'to be', 'Success')
-    })
-
-    after(async () => {
-      code3 = (await userDao.get({ phone })).code
-      expect(code3, 'to be ok')
-    })
-  })
-
-  describe('PUT: change pin for existing user', () => {
-    it('verify a new pin', async () => {
-      expect(code2, 'not to be', code3)
-      const response = await client.put(`/dev/v1/key/${keyId}`, {
-        body: {
-          phone,
-          code: code3,
-          op: 'read',
-          pin: pin1,
-          newPin: pin2
-        }
-      })
-      expect(response.status, 'to be', 200)
-      expect(response.body.encryptionKey, 'to be ok')
-    })
-  })
-
-  describe('POST: create key for existing phone number', () => {
-    it('return new dummy key id to preserve user privacy', async () => {
-      const response = await client.post('/dev/v1/key', {
-        body: { phone }
-      })
-      expect(response.body.id, 'to be ok')
-      expect(response.body.id, 'not to equal', keyId)
-      expect(response.status, 'to be', 201)
-    })
-  })
-
-  describe('DELETE: request key removal', () => {
-    it('delete key document', async () => {
-      const response = await client.delete(`/dev/v1/key/${keyId}`, {
-        params: { phone }
-      })
-      expect(response.status, 'to be', 200)
-      expect(response.body.message, 'to be', 'Success')
-    })
-
-    after(async () => {
-      code4 = (await userDao.get({ phone })).code
-      expect(code4, 'to be ok')
-      expect(code3, 'not to be', code4)
-    })
-  })
-
-  describe('PUT: old pin no longer works', () => {
-    it('return 404', async () => {
-      const response = await client.put(`/dev/v1/key/${keyId}`, {
-        body: {
-          phone,
-          code: code4,
-          op: 'remove',
-          pin: pin1
-        }
-      })
+  describe('DELETE: remove user', () => {
+    it('not delete with wrong pin', async () => {
+      client.auth('', pin1)
+      const response = await client.delete(`/v1/key/${keyId}/user/${phone}`)
       expect(response.status, 'to be', 404)
-      expect(response.body.message, 'to be', 'Invalid params')
-      const user = await userDao.get({ phone })
-      expect(user.invalidCount, 'to be', 1)
     })
-  })
 
-  describe('PUT: verify key removal', () => {
-    it('verify a different code', async () => {
-      const response = await client.put(`/dev/v1/key/${keyId}`, {
-        body: {
-          phone,
-          code: code4,
-          op: 'remove',
-          pin: pin2
-        }
-      })
+    it('delete user with correct pin', async () => {
+      client.auth('', pin2)
+      const response = await client.delete(`/v1/key/${keyId}/user/${phone}`)
       expect(response.status, 'to be', 200)
-      expect(response.body.message, 'to be', 'Success')
-      const user = await userDao.get({ phone })
-      expect(user, 'to be', null)
     })
   })
 })
